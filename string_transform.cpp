a882b1e855a443d91d0c6d6d9224875cb1bcdd6e
Abrar Shariar <shariarabrar@gmail.com>
2016-01-14 17:40:11 -0800

    Sample input

--- string_transform.cpp
+++ string_transform.cpp
@@ -1,1 +1,47 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+using namespace std;
+
+int main(){
+    ifstream read;
+    read.open("input.txt");
+
+     string text;
+     string temp;
+     getline(read,text);
+     cout<<text<<endl;
+
+     //cin>>text;
+     int test;
+     cin>>test;
+     for(int i=0;i<test;i++){
+            int f1,f2;
+            cin>>f1>>f2;
+            temp=text.substr(f1,f2+1);
+            //reverse temp
+            string rev="";
+            for(int i=temp.length()-1;i>=0;i--){
+                rev.push_back(temp[i]);
+                    }
+            //cout<<rev;
+            //replace text
+            text.replace(f1,f2+1,rev);
+            cout<<text<<endl;
+     }
+     read.close();
+}
+
+    /*
+    string text="Hello World";
+    string reStr="C++";
+    text.replace(0,5,reStr);
+    cout<<text<<endl;
+    */
+
+
+
+
+
+
 

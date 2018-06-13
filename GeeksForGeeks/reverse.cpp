6010d701c71fcc2721cfd23fe1181f40a9baad96
abrarShariar <shariarabrar@gmail.com>
2018-06-13 14:23:59 +0600

    reverse exluding special chars

---
+++ GeeksForGeeks/reverse.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int T;
+    cin>>T;
+    for(int i=0;i<T;i++){
+        string s;
+        cin>>s;
+
+        //reverse (no swap, no move)
+        int si = 0;
+        int ei = s.length() - 1;
+
+        while(si < ei){
+            int sEl = (int) s[si];
+            int eEl = (int) s[ei];
+            if((sEl >= 65 && sEl <= 90) || (sEl >= 97 && sEl <= 122)){
+              if((eEl >= 65 && eEl <= 90) || (eEl >= 97 && eEl <= 122)){
+                //swap
+                int temp = s[si];
+                s[si] = s[ei];
+                s[ei] = temp;
+                si++;
+                ei--;
+                continue;
+              }
+              ei--;
+            } else {
+                si++;
+            }
+        }
+
+        cout<<s<<endl;
+    }
+
+
+}
+

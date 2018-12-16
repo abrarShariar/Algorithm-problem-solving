da72c4856fe2f644b2658397710c3d0a6d586a2b
abrarShariar <shariarabrar@gmail.com>
2018-12-16 13:39:53 +0600

    array man

--- hackerRank/array_manipulation.py
+++ hackerRank/array_manipulation.py
@@ -1,12 +1,18 @@
-# input
-n,m = map(int,raw_input().split())
-array = [0 for _ in range(n+1)]
+#!/bin/python3
 
-# calculation
-for index in range(0,m):
-    a,b,k = map(int,raw_input().split())
-    for i in range(a,b+1):
-        array[i] = array[i] + k
-    
-# output 
-print max(array)
+def arrayManipulation(n, queries):
+    arr = [0]*(n+1)
+    # take input for queries and update
+    for i in range(queries):
+        start, end, val = input().strip().split(' ')
+        start, end, val = [int(start), int(end), int(val)]
+        for j in range(start-1, end):
+            arr[j] += val
+
+    # find the max
+    return max(arr)
+
+n, m = input().strip().split(' ')
+n, m = [int(n), int(m)]
+result = arrayManipulation(n, m)
+print(result)

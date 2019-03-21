fcd092840186f28132bb5bd1a01a1c78b5aa802c
Abrar Shariar <shariarabrar@gmail.com>
2019-03-21 21:55:29 +0900

    temp world

--- codeforces/color_boots.cpp
+++ codeforces/color_boots.cpp
@@ -34,29 +34,72 @@ int main () {
   map<char, vector<int> >::iterator searchIt;
   for (it = firstBoot.begin(); it != firstBoot.end(); it++) {
     char testChar = it->first;
-    searchIt = secondBoot.find(testChar);
-    cout<<it->first<<" "<<searchIt->first<<endl;
-    if (searchIt != secondBoot.end()) {
-      while (it->second.size() > 0) {
-        if (searchIt->second.size() > 0) {
-          int f1 = it->second.back();
-          int f2 = searchIt->second.back();
-          it->second.pop_back();
-          searchIt->second.pop_back();
-          firstBootIndex.push_back(f1);
-          secondBootIndex.push_back(f2);
-          countPairs++;
-        } else {
-          break;
+    if (testChar != '?') {
+      searchIt = secondBoot.find(testChar);
+      // cout<<it->first<<" "<<searchIt->first<<endl;
+      if (searchIt != secondBoot.end()) {
+        while (it->second.size() > 0) {
+          if (searchIt->second.size() > 0) {
+            int f1 = it->second.back();
+            int f2 = searchIt->second.back();
+            it->second.pop_back();
+            searchIt->second.pop_back();
+            firstBootIndex.push_back(f1);
+            secondBootIndex.push_back(f2);
+            countPairs++;
+          } else {
+            break;
+          }
         }
       }
     }
   }
 
-
+  // cout<<countPairs<<endl;
   // filter the question marks
   it = firstBoot.find('?');
-  
+  searchIt = secondBoot.find('?');
+  // cout<<it->first<<endl;
+  if (it != firstBoot.end()) {
+    // ? marks found in firstBoot
+    for (map<char, vector<int> >::iterator s = secondBoot.begin();s != secondBoot.end(); s++) {
+      if (it->second.size() <= 0) {
+        break;
+      }
+      if (s->first != '?') {
+        while (s->second.size() > 0 && it->second.size() > 0) {
+          int f1 = it->second.back();
+          int f2 = s->second.back();
+          firstBootIndex.push_back(f1);
+          secondBootIndex.push_back(f2);
+          it->second.pop_back();
+          s->second.pop_back();
+          countPairs++;
+        }
+      }
+    }
+  }
+
+  if (searchIt != secondBoot.end()) {
+    // ? marks found in secondBoot
+    for (map<char, vector<int> >::iterator s = firstBoot.begin();s != firstBoot.end(); s++) {
+      if (searchIt->second.size() <= 0) {
+        break;
+      }
+      if (s->first != '?') {
+        while (s->second.size() > 0 && searchIt->second.size() > 0) {
+          int f1 = searchIt->second.back();
+          int f2 = s->second.back();
+          firstBootIndex.push_back(f2);
+          secondBootIndex.push_back(f1);
+          searchIt->second.pop_back();
+          s->second.pop_back();
+          countPairs++;
+        }
+      }
+    }
+  }
+
   // output print
   cout<<countPairs<<endl;
   for (int i=0;i<firstBootIndex.size();i++) {

65bcb9ec59988aeacf0ce669a10c90fd9ea36991
Abrar Shariar <shariarabrar@gmail.com>
2020-04-29 23:44:40 +0900

    in progress

---
+++ interviewCake/word_cloud.py
@@ -0,0 +1,91 @@
+import unittest
+
+
+class WordCloudData(object):
+
+    def __init__(self, input_string):
+        start = 0
+        w_length = 0
+        self.words_to_counts = {}
+        for i in range(len(input_string)):
+            if input_string[i].isalpha() or input_string[i] == "'" or input_string[i] == ":" or (input_string[i] == "-" and input_string[i-1] != " "):
+                if w_length == 0:
+                    start = i
+                w_length += 1
+            
+            elif input_string[i] == " " or input_string[i] == "?" or input_string[i] == ":" or input_string[i] == ".":
+                word = input_string[start:start+(w_length-1)].lower()
+                self.words_to_counts[word] = self.words_to_counts.get(word, 0) + 1
+                w_length = 0
+
+# Tests
+# There are lots of valid solutions for this one. You
+# might have to edit some of these tests if you made
+# different design decisions in your solution.
+
+class Test(unittest.TestCase):
+
+    def test_simple_sentence(self):
+        input = 'I like cake'
+
+        word_cloud = WordCloudData(input)
+        actual = word_cloud.words_to_counts
+
+        expected = {'I': 1, 'like': 1, 'cake': 1}
+        self.assertEqual(actual, expected)
+
+    def test_longer_sentence(self):
+        input = 'Chocolate cake for dinner and pound cake for dessert'
+
+        word_cloud = WordCloudData(input)
+        actual = word_cloud.words_to_counts
+
+        expected = {
+            'and': 1,
+            'pound': 1,
+            'for': 2,
+            'dessert': 1,
+            'Chocolate': 1,
+            'dinner': 1,
+            'cake': 2,
+        }
+        self.assertEqual(actual, expected)
+
+    def test_punctuation(self):
+        input = 'Strawberry short cake? Yum!'
+
+        word_cloud = WordCloudData(input)
+        actual = word_cloud.words_to_counts
+
+        expected = {'cake': 1, 'Strawberry': 1, 'short': 1, 'Yum': 1}
+        self.assertEqual(actual, expected)
+
+    def test_hyphenated_words(self):
+        input = 'Dessert - mille-feuille cake'
+
+        word_cloud = WordCloudData(input)
+        actual = word_cloud.words_to_counts
+
+        expected = {'cake': 1, 'Dessert': 1, 'mille-feuille': 1}
+        self.assertEqual(actual, expected)
+
+    def test_ellipses_between_words(self):
+        input = 'Mmm...mmm...decisions...decisions'
+
+        word_cloud = WordCloudData(input)
+        actual = word_cloud.words_to_counts
+
+        expected = {'mmm': 2, 'decisions': 2}
+        self.assertEqual(actual, expected)
+
+    def test_apostrophes(self):
+        input = "Allie's Bakery: Sasha's Cakes"
+
+        word_cloud = WordCloudData(input)
+        actual = word_cloud.words_to_counts
+
+        expected = {"Bakery": 1, "Cakes": 1, "Allie's": 1, "Sasha's": 1}
+        self.assertEqual(actual, expected)
+
+
+unittest.main(verbosity=2)

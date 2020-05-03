8212fa80ca6d97d115f69d3ec248c6c6a9e9b0db
Abrar Shariar <shariarabrar@gmail.com>
2020-05-03 20:29:35 +0900

    2nd largest

---
+++ interviewCake/second_largest.py
@@ -0,0 +1,162 @@
+
+import unittest
+
+# def find_second_largest(root_node):
+
+#     # Find the second largest item in the binary search tree
+#     if root_node == None:
+#         return
+
+#     elif root_node.right == None and root_node.left != None:
+#         return root_node.left.value
+    
+#     elif root_node.left == None and root_node.right != None:
+#         return root_node.value
+
+#     return find_second_largest(root_node.right)
+
+
+def find_second_largest(root):
+    if root == None:
+        return
+    
+    que = []
+    # root, left, right
+    mx = [root.value, 0, 0]
+    que.append(root)
+
+    while len(que):
+        node = que.pop(0)
+        mx[0] = node.value
+        if node.left != None:
+            que.append(node.left)
+            mx[1] = node.left.value
+        else:
+            mx[1] = 0
+        if node.right != None:
+            que.append(node.right)
+            mx[2] = node.right.value 
+        else:
+            mx[2] = 0
+
+
+    mx.sort()
+    return mx[1]
+
+# Tests
+class Test(unittest.TestCase):
+
+    class BinaryTreeNode(object):
+
+        def __init__(self, value):
+            self.value = value
+            self.left = None
+            self.right = None
+
+        def insert_left(self, value):
+            self.left = Test.BinaryTreeNode(value)
+            return self.left
+
+        def insert_right(self, value):
+            self.right = Test.BinaryTreeNode(value)
+            return self.right
+
+    def test_full_tree(self):
+        tree = Test.BinaryTreeNode(50)
+        left = tree.insert_left(30)
+        right = tree.insert_right(70)
+        left.insert_left(10)
+        left.insert_right(40)
+        right.insert_left(60)
+        right.insert_right(80)
+        actual = find_second_largest(tree)
+        expected = 70
+        self.assertEqual(actual, expected)
+
+    def test_largest_has_a_left_child(self):
+        tree = Test.BinaryTreeNode(50)
+        left = tree.insert_left(30)
+        right = tree.insert_right(70)
+        left.insert_left(10)
+        left.insert_right(40)
+        right.insert_left(60)
+        actual = find_second_largest(tree)
+        expected = 60
+        self.assertEqual(actual, expected)
+
+    def test_largest_has_a_left_subtree(self):
+        tree = Test.BinaryTreeNode(50)
+        left = tree.insert_left(30)
+        right = tree.insert_right(70)
+        left.insert_left(10)
+        left.insert_right(40)
+        right_left = right.insert_left(60)
+        right_left_left = right_left.insert_left(55)
+        right_left.insert_right(65)
+        right_left_left.insert_right(58)
+        actual = find_second_largest(tree)
+        expected = 65
+        self.assertEqual(actual, expected)
+
+    def test_second_largest_is_root_node(self):
+        tree = Test.BinaryTreeNode(50)
+        left = tree.insert_left(30)
+        tree.insert_right(70)
+        left.insert_left(10)
+        left.insert_right(40)
+        actual = find_second_largest(tree)
+        expected = 50
+        self.assertEqual(actual, expected)
+
+    def test_descending_linked_list(self):
+        tree = Test.BinaryTreeNode(50)
+        left = tree.insert_left(40)
+        left_left = left.insert_left(30)
+        left_left_left = left_left.insert_left(20)
+        left_left_left.insert_left(10)
+        actual = find_second_largest(tree)
+        expected = 40
+        self.assertEqual(actual, expected)
+
+    def test_ascending_linked_list(self):
+        tree = Test.BinaryTreeNode(50)
+        right = tree.insert_right(60)
+        right_right = right.insert_right(70)
+        right_right.insert_right(80)
+        actual = find_second_largest(tree)
+        expected = 70
+        self.assertEqual(actual, expected)
+
+    def test_error_when_tree_has_one_node(self):
+        tree = Test.BinaryTreeNode(50)
+        with self.assertRaises(Exception):
+           find_second_largest(tree)
+
+    def test_error_when_tree_is_empty(self):
+        with self.assertRaises(Exception):
+           find_second_largest(None)
+
+
+unittest.main(verbosity=2)      
+
+# class BinaryTreeNode:
+#     def __init__(self, value):
+#         self.value = value
+#         self.left = None
+#         self.right = None
+
+#     def insert_left(self, value):
+#         self.left = BinaryTreeNode(value)
+#         return self.left
+
+#     def insert_right(self, value):
+#         self.right = BinaryTreeNode(value)
+#         return self.right
+
+# tree = BinaryTreeNode(60)
+# left = tree.insert_left(55)
+# right = tree.insert_right(70)
+# right.insert_left(65)
+# # right.insert_right(79)
+
+# find_second_largest(tree)

d589b6eb1b80dffc34d294fb214e11ce406d0023
Abrar Shariar <shariarabrar@gmail.com>
2020-09-06 14:32:05 +0900

    Merge meeting 2

---
+++ interviewCake/merge_meeting_place.py
@@ -0,0 +1,65 @@
+import unittest
+
+def merge_ranges(meetings):
+	if len(meetings) == 0:
+		return meetings
+
+	meetings.sort(key=lambda x: x[0])
+	merged_list = [(meetings[0])]
+
+	for current_start_time, current_end_time in meetings[1:]:
+		# check if the current start time is less than the end time of the latest end time in the merged list
+		last_merged_start, last_merged_end = merged_list[-1]
+
+		if current_start_time <= last_merged_end:
+			merged_list[-1] = (last_merged_start, max(current_end_time, last_merged_end))
+		else:
+			merged_list.append((current_start_time, current_end_time))
+
+	return merged_list
+	
+		
+class Test(unittest.TestCase):
+
+    def test_meetings_overlap(self):
+        actual = merge_ranges([(1, 3), (2, 4)])
+        expected = [(1, 4)]
+        self.assertEqual(actual, expected)
+
+    def test_meetings_touch(self):
+        actual = merge_ranges([(5, 6), (6, 8)])
+        expected = [(5, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_meeting_contains_other_meeting(self):
+        actual = merge_ranges([(1, 8), (2, 5)])
+        expected = [(1, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_meetings_stay_separate(self):
+        actual = merge_ranges([(1, 3), (4, 8)])
+        expected = [(1, 3), (4, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_multiple_merged_meetings(self):
+        actual = merge_ranges([(1, 4), (2, 5), (5, 8)])
+        expected = [(1, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_meetings_not_sorted(self):
+        actual = merge_ranges([(5, 8), (1, 4), (6, 8)])
+        expected = [(1, 4), (5, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_one_long_meeting_contains_smaller_meetings(self):
+        actual = merge_ranges([(1, 10), (2, 5), (6, 8), (9, 10), (10, 12)])
+        expected = [(1, 12)]
+        self.assertEqual(actual, expected)
+
+    def test_sample_input(self):
+        actual = merge_ranges([(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)])
+        expected = [(0, 1), (3, 8), (9, 12)]
+        self.assertEqual(actual, expected)
+
+
+unittest.main(verbosity=2)

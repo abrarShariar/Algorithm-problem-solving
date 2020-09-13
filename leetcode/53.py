c7510effe04d3283478f09bf83ea980b67aa1ef8
Abrar Shariar <shariarabrar@gmail.com>
2020-09-13 18:48:03 +0900

    Max product

--- leetcode/53.py
+++ leetcode/53.py
@@ -5,14 +5,10 @@ class Solution:
 			
 			for i in range(1, len(nums)):
 				# update current streak
-				if current_streak + nums[i] > nums[i]:
-					current_streak = current_streak + nums[i]
-				else:
-					current_streak = nums[i]
+				current_streak = max(nums[i], current_streak + nums[i])
 
 				# update global streak
-				if current_streak > global_streak:
-					global_streak = current_streak 
+				global_streak = max(global_streak, current_streak)
 			
 			return global_streak
 

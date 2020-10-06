ac533e256d08ef4d1b66cb6defc0522a6099a273
Abrar Shariar <shariarabrar@gmail.com>
2020-10-06 19:40:42 +0900

    Add test case

--- interviewCake/mesh-message.py
+++ interviewCake/mesh-message.py
@@ -1,3 +1,5 @@
+import unittest
+
 def get_path(graph, start_node, end_node):
   visited = [start_node]
   queue = [start_node]
@@ -40,23 +42,65 @@ def get_path(graph, start_node, end_node):
   shortest_path.append(end_node)
   print(shortest_path)
 
-graph_input = {
-  'j': ['m', 'b', 'r', 'n'],
-  'm': ['w', 'o']
-}
 
-network = {
-  'Min'     : ['William', 'Jayden', 'Omar'],
-  'William' : ['Min', 'Noam'],
-  'Jayden'  : ['Min', 'Amelia', 'Ren', 'Noam'],
-  'Ren'     : ['Jayden', 'Omar'],
-  'Amelia'  : ['Jayden', 'Adam', 'Miguel'],
-  'Adam'    : ['Amelia', 'Miguel', 'Sofia', 'Lucas'],
-  'Miguel'  : ['Amelia', 'Adam', 'Liam', 'Nathan'],
-  'Noam'    : ['Nathan', 'Jayden', 'William'],
-  'Omar'    : ['Ren', 'Min', 'Scott'],
-}
 
-get_path(network, 'Jayden', 'Adam')
+# Tests
 
-  
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        self.graph = {
+            'a': ['b', 'c', 'd'],
+            'b': ['a', 'd'],
+            'c': ['a', 'e'],
+            'd': ['a', 'b'],
+            'e': ['c'],
+            'f': ['g'],
+            'g': ['f'],
+        }
+
+    def test_two_hop_path_1(self):
+        actual = get_path(self.graph, 'a', 'e')
+        expected = ['a', 'c', 'e']
+        self.assertEqual(actual, expected)
+
+    def test_two_hop_path_2(self):
+        actual = get_path(self.graph, 'd', 'c')
+        expected = ['d', 'a', 'c']
+        self.assertEqual(actual, expected)
+
+    def test_one_hop_path_1(self):
+        actual = get_path(self.graph, 'a', 'c')
+        expected = ['a', 'c']
+        self.assertEqual(actual, expected)
+
+    def test_one_hop_path_2(self):
+        actual = get_path(self.graph, 'f', 'g')
+        expected = ['f', 'g']
+        self.assertEqual(actual, expected)
+
+    def test_one_hop_path_3(self):
+        actual = get_path(self.graph, 'g', 'f')
+        expected = ['g', 'f']
+        self.assertEqual(actual, expected)
+
+    def test_zero_hop_path(self):
+        actual = get_path(self.graph, 'a', 'a')
+        expected = ['a']
+        self.assertEqual(actual, expected)
+
+    def test_no_path(self):
+        actual = get_path(self.graph, 'a', 'f')
+        expected = None
+        self.assertEqual(actual, expected)
+
+    def test_start_node_not_present(self):
+        with self.assertRaises(Exception):
+            get_path(self.graph, 'h', 'a')
+
+    def test_end_node_not_present(self):
+        with self.assertRaises(Exception):
+            get_path(self.graph, 'a', 'h')
+
+
+unittest.main(verbosity=2)

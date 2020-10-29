a5fd39aa68d9233434478156fbba8ababd1dd260
Abrar Shariar <shariarabrar@gmail.com>
2020-10-29 21:37:57 +0900

    Algo DS micromasters

---
+++ Algo-DS-Micromasters/algorithm-design-technique/max_pairwise_sum.py
@@ -0,0 +1,6 @@
+def max_pairwise(num_list):
+	num_list.sort()
+	return num_list[len(num_list) - 1] * num_list[len(num_list) - 2]
+
+input_list = list(map(int, input().split(' ')))
+print(max_pairwise(input_list))
---
+++ Algo-DS-Micromasters/algorithm-design-technique/sum.py
@@ -0,0 +1,6 @@
+import sys
+
+input = sys.stdin.read()
+tokens = input.split()
+
+print(int(tokens[0]) + int(tokens[1]))

2d89843dd219b551a32bbbb76af6aa47c89893cb
Abrar Shariar <shariarabrar@gmail.com>
2021-01-24 16:30:29 +0900

    Max sum with test cases

--- python/max_sum.py
+++ python/max_sum.py
@@ -1,5 +1,7 @@
-# WRONG
 def get_max_even_sum (input_arr, k):
+    if k > len(input_arr) or len(input_arr) == 0:
+        return -1
+
     even_arr = []
     odd_arr = []
     # sort the arr
@@ -16,15 +18,18 @@ def get_max_even_sum (input_arr, k):
     even_index, odd_index = 0, 0
     max_sum = 0
 
-    print(even_arr)
-    print(odd_arr)
+    # print(even_arr)
+    # print(odd_arr)       
 
     # Go Greedy
     while k > 0:
         if k % 2 == 1:
-            max_sum += even_arr[even_index]
-            even_index += 1
-            k -= 1
+            if (len(even_arr) > 0):
+                max_sum += even_arr[even_index]
+                even_index += 1
+                k -= 1
+            else:
+                return -1
         else:
             if even_index < len(even_arr) - 1 and odd_index < len(odd_arr) - 1:
                 max_sum += max(even_arr[even_index] + even_arr[even_index + 1], odd_arr[odd_index] + odd_arr[odd_index + 1])
@@ -33,7 +38,7 @@ def get_max_even_sum (input_arr, k):
             elif even_index < len(even_arr) - 1:
                 max_sum += even_arr[even_index] + even_arr[even_index + 1]
                 even_index += 2
-            else:
+            elif odd_index < len(odd_arr) - 1:
                 max_sum += odd_arr[odd_index] + odd_arr[odd_index + 1]
                 odd_index += 2
             
@@ -42,7 +47,33 @@ def get_max_even_sum (input_arr, k):
     return max_sum        
     
 
-print(get_max_even_sum([4,2,6,7,8], 3))
+# print(get_max_even_sum([4,2,6,7,8], 3))
+# print(get_max_even_sum([1,1,1], 3))
+# print(get_max_even_sum([1,1], 3))
+# print(get_max_even_sum([1], 3))
+# print(get_max_even_sum([1], 3))
+# print(get_max_even_sum([], 0))
+# print(get_max_even_sum([1,2], 1))
+# print(get_max_even_sum([1,2], 2))
+
+# print(get_max_even_sum([ 2, 4, 10, 3, 5 ], 3))
+# print(get_max_even_sum([ 5,5,1,1,3 ], 3))
+
+# print(get_max_even_sum([4,2,6,7,8], 3))
+# print(get_max_even_sum([5, 5, 2, 4, 3], 3))
+
+# print(get_max_even_sum([-1,-2,-2], 3))
+
+# print(get_max_even_sum([1,2,4,11], 100))
+# print(get_max_even_sum([1,2,4,11], 1))
+
+
+
+
+
+
+
+
 
     
     

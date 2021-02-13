27cac601817067afde23f65b9b0eda957eac0137
Abrar Shariar <shariarabrar@gmail.com>
2021-02-13 22:25:16 +0900

    DKS

---
+++ algoexpert/coding-problems/djkstra/index.py
@@ -0,0 +1,63 @@
+import math
+
+
+# O(V^2) + O(e)
+# 
+
+def dijkstrasAlgorithm(start, edges): 
+    
+    # total number of vertices
+    total_vertices = len(edges)
+    # a min distance list to store the min distance to all nodes
+    minDistance = [float('inf') for i in range(total_vertices)]
+    # set min distance of start node to 0
+    minDistance[start] = 0
+    visited = set()
+
+    # loop over each node until all is visited 
+    while len(visited) != total_vertices:
+        # get the node with minimum distance => visit it
+        currentNode, currentMinDistance = getNextMinVertex(visited, minDistance)
+        minDistance[currentNode] = currentMinDistance
+
+        if currentMinDistance == float('inf'):
+            break
+        
+        visited.add(currentNode)
+
+        # visit all unvisited neighbours of currentNode
+        for neighbour in edges[currentNode]:
+            node, distance = neighbour 
+
+            if node not in visited:
+                current_distance = distance + minDistance[currentNode]
+                if current_distance < minDistance[node]:
+                    minDistance[node] = current_distance
+
+    return [-1 if val == float('inf') else val for val in minDistance]
+
+def getNextMinVertex(visited, distance):
+    currentMinDistance = float('inf')
+    currentMinVertex = None
+
+    for current_node in range(len(distance)):
+        if current_node in visited:
+            continue
+
+        if distance[current_node] <= currentMinDistance:
+            currentMinDistance = distance[current_node]
+            currentMinVertex = current_node
+    
+    return currentMinVertex, currentMinDistance
+
+start = 0
+edges = [
+    [[1,7]],
+    [[2,6], [3,20], [4,3]],
+    [[3,14]],
+    [[4,2]],
+    [],
+    []
+]
+
+print(dijkstrasAlgorithm(start, edges))

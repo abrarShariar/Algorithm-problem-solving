9fd8b81e7175a6277f51ca36c9e40aa3dfdf5650
Abrar Shariar <shariarabrar@gmail.com>
2021-03-14 00:21:55 +0900

    Divide and conquer

---
+++ Algo-DS-Micromasters/algorithm-design-technique/divide-and-conquer/quicksort.py
@@ -0,0 +1,45 @@
+# QUCIKSORT
+
+# the partition steps in own words:
+# 1. take the leftmost item as the pivot
+# 2. create two pointers => i for the left + 1 and j = left
+# 3. loop with the i index till the end
+# 4. our goal is to make two separate windows
+    # 1. the j index tracks the items which are less than pivot
+    # 2. the i index tracks the items which are greter than pivot
+# 5. so anytime we find an item in A[i] < pivot => swap with the A[j+1]...since j is the last item which is less than pivot
+# 6. at last we swap the leftmost item (the pivot) with the j (since the A[j] is ofcourse less than the pivot)
+
+def partition(A, left, right):
+    # take the pivot
+    pivot = A[left]
+    i = left + 1
+    j = left
+    while i < right:
+        if A[i] <= pivot:
+            j += 1
+            A[i], A[j] = A[j], A[i]
+        
+        i+= 1
+    
+    # swap the pivot
+    A[left], A[j] = A[j], A[left]
+    # return j since j is the index where the pivot is located
+    return j
+
+# print(partition(arr, 0, len(arr)))
+def quick_sort(A, left, right):
+    if left < right:
+        # find the partition point
+        m = partition(A, left, right)
+        quick_sort(A, left, m)
+        quick_sort(A, m+1, right)
+
+# quick sort is an in place sorting algorithm => the input list is updated
+input_list = [6,4,2,3,9,8,9,4,7,6,1]
+quick_sort(input_list, 0, len(input_list))
+print(input_list)
+
+
+    
+
--- educative/sliding-window-problems/word_concat.py
+++ educative/sliding-window-problems/word_concat.py
@@ -1,55 +1,91 @@
-# IN PROGRESS
+# def find_word_concatenation(str1, words):
+#   if len(words) == 0 or len(words[0]) == 0:
+#     return []
+
+#   word_frequency = {}
+
+#   for word in words:
+#     if word not in word_frequency:
+#       word_frequency[word] = 0
+#     word_frequency[word] += 1
+
+#   result_indices = []
+#   words_count = len(words)
+#   word_length = len(words[0])
+
+#   for i in range((len(str1) - words_count * word_length)+1):
+#     words_seen = {}
+#     for j in range(0, words_count):
+#       next_word_index = i + j * word_length
+#       # Get the next word from the string
+#       word = str1[next_word_index: next_word_index + word_length]
+#       if word not in word_frequency:  # Break if we don't need this word
+#         break
+
+#       # Add the word to the 'words_seen' map
+#       if word not in words_seen:
+#         words_seen[word] = 0
+#       words_seen[word] += 1
+
+#       # No need to process further if the word has higher frequency than required
+#       if words_seen[word] > word_frequency.get(word, 0):
+#         break
+
+#       if j + 1 == words_count:  # Store index if we have found all the words
+#         result_indices.append(i)
+
+#   return result_indices
+
+
+# def main():
+#   print(find_word_concatenation("catfoxcat", ["cat", "fox"]))
+#   print(find_word_concatenation("catcatfoxfox", ["cat", "fox"]))
+
+
+# main()
+
+
+
 def find_word_concatenation(str, words):
     result_indices = []
-
-    word_len = len(words[0])
-    window_size = (len(words) * word_len)
-
-    # create the dictionary from the words list
     word_dict = {}
+    
+    # create the dictionary with words
     for word in words:
-        for ch in word:
-            word_dict[ch] = word_dict.get(ch, 0) + 1
+        word_dict[word] = word_dict.get(word, 0) + 1
 
+    # initialize the words seen
+    words_seen = {}
+
+    # define variables for word_count and word_length
+    total_words = len(words)
+    word_length = len(words[0])
 
     window_start = 0
-    matched = 0
-    for window_end in range(len(str)):
-        left_char = str[window_start]
-        right_char = str[window_end]
-        current_window_size = window_end - window_start + 1
+    window_end = word_length - 1
 
-        # if the window size is large => shirnk
-        if current_window_size > window_size:
-            # shrink steps goes here
-            if left_char in word_dict:
-                word_dict[left_char] += 1
-                window_start += 1
-                if word_dict[left_char] == 0:
-                    matched -= 1
+    while window_end < len(str):
+        # slice out the current word
+        current_word = str[window_start: window_end + 1]
+
+        # check if the current word is part of the word_dict
+        if current_word in word_dict:
+            words_seen[current_word] = words_seen.get(current_word, 0) + 1
+
+            # if the words_seen is equal to the words => updated matched
+            if words_seen[current_word] == word_dict[current_word]:
+                result_indices.append(window_start)
+
+            # if we got a word...go for the next
+            window_start = window_end 
+            window_end = (window_start + word_length) - 1
+        
         else:
-        # if window size is smaller => grow
-            if right_char in word_dict:
-                if word_dict[right_char] == 0:
-                    if left_char in word_dict:
-                        word_dict[left_char] += 1
-                        window_start += 1
-                        if word_dict[left_char] == 0:
-                            matched += 1
-                    
-                word_dict[right_char] -= 1
+            window_end = (window_start + word_length) - 1
+            window_start += 1
 
-        if word_dict[right_char] == 0:
-            matched += 1
-
-        if matched == len(word_dict):
-            result_indices.append(window_start)
-            matched = 0
-
-    if matched == len(word_dict):
-        result_indices.append(window_start)
 
     return result_indices
 
-# print(find_word_concatenation("catcatfoxfox", ["cat", "fox"]))
+
 print(find_word_concatenation("catfoxcat", ["cat", "fox"]))

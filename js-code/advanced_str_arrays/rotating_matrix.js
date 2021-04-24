60e49232ba2dbd450add3abc736504208653963a
Abrar Shariar <shariarabrar@gmail.com>
2021-04-24 22:37:53 +0900

    Add code for rotating matrix

--- js-code/advanced_str_arrays/rotating_matrix.js
+++ js-code/advanced_str_arrays/rotating_matrix.js
@@ -1,23 +1,34 @@
+// function rotateMatrix (mat) {
+//     const columnLimit = mat[0].length
+//     const rowLimit = mat.length
+//     resultMat = []
+
+//     for (let column = 0; column < columnLimit; column++) {
+//         const newMat = []
+//         for (let row = rowLimit - 1; row >= 0; row--) {
+//             newMat.push(mat[row][column])
+//         }
+//         resultMat.push(newMat)
+//     }
+//     return resultMat
+// }
+
 function rotateMatrix (mat) {
     const columnLimit = mat[0].length
     const rowLimit = mat.length
-    resultMat = []
 
     for (let column = 0; column < columnLimit; column++) {
-        const newMat = []
         for (let row = rowLimit - 1; row >= 0; row--) {
-            newMat.push(mat[row][column])
+            mat[column][(rowLimit - 1) - row], mat[row][column] = mat[row][column], mat[column][(rowLimit - 1) - row]
         }
-        resultMat.push(newMat)
     }
-    return resultMat
+    return mat
 }
 
 input_mat = [
-    [1,2,3,10],
-    [4,5,6,11],
-    [7,8,9,12],
-    [0,0,0,0]
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
 ]
 
 console.log(rotateMatrix(input_mat))

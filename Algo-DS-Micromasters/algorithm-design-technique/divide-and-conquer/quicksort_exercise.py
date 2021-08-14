c53d419099046fad791a07947bf9a3ac11982e52
Abrar Shariar <shariarabrar@gmail.com>
2021-08-14 20:25:49 +0900

    3 way partition quicksort

---
+++ Algo-DS-Micromasters/algorithm-design-technique/divide-and-conquer/quicksort_exercise.py
@@ -0,0 +1,51 @@
+# Uses python3
+import sys
+import random
+
+def partition3(A, l, r):
+	lt = l
+	gt = r
+	i = l
+	pivot = A[l]
+
+	while i <= gt:
+		if A[i] < pivot:
+			A[i], A[lt] = A[lt], A[i]
+			lt += 1
+			i += 1
+		elif A[i] > pivot:
+			A[i], A[gt] = A[gt], A[i]
+			gt -= 1 
+		else:
+			i += 1 
+
+	return lt, gt
+
+def partition2(a, l, r):
+	x = a[l]
+	j = l;
+	for i in range(l + 1, r + 1):
+		if a[i] <= x:
+			j += 1
+			a[i], a[j] = a[j], a[i]
+	a[l], a[j] = a[j], a[l]
+	return j
+
+
+def randomized_quick_sort(a, l, r):
+	if l >= r:
+		return
+	k = random.randint(l, r)
+	a[l], a[k] = a[k], a[l]
+	#use partition3
+	m1, m2 = partition3(a, l, r)
+	randomized_quick_sort(a, l, m1 - 1);
+	randomized_quick_sort(a, m2 + 1, r);
+
+
+if __name__ == '__main__':
+	input = sys.stdin.read()
+	n, *a = list(map(int, input.split()))
+	randomized_quick_sort(a, 0, n - 1)
+	for x in a:
+		print(x, end=' ')
--- Algo-DS-Micromasters/algorithm-design-technique/divide-and-conquer/quicksort_optimized.py
+++ Algo-DS-Micromasters/algorithm-design-technique/divide-and-conquer/quicksort_optimized.py
@@ -5,10 +5,10 @@ def qs(item_list, left_index, right_index):
 		return 
 
 	# get the pivot
-	m = get_pivot(item_list, left_index, right_index)
+	m1, m2 = get_pivot3(item_list, left_index, right_index)
 
-	qs(item_list, left_index, m - 1)
-	qs(item_list, m+1, right_index)
+	qs(item_list, left_index, m1 - 1)
+	qs(item_list, m2 + 1, right_index)
 
 def get_pivot(item_list, left_index, right_index):
 	# get a random pivot index
@@ -28,11 +28,30 @@ def get_pivot(item_list, left_index, right_index):
 	item_list[j], item_list[pivot_index] = item_list[pivot_index], item_list[j]
 	return j
 
+def get_pivot3(A, l, r):
+	lt = l
+	gt = r
+	i = l
+	pivot = A[l]
+
+	while i <= gt:
+		if A[i] < pivot:
+			A[i], A[lt] = A[lt], A[i]
+			lt += 1
+			i += 1
+		elif A[i] > pivot:
+			A[i], A[gt] = A[gt], A[i]
+			gt -= 1 
+		else:
+			i += 1 
+
+	return lt, gt
+
 def get_random(l, r):
 	return random.randint(l, r)
 
 
-ll = [10,9,8,7,5,3,1,100,200]
+ll = [2,3,9,2,2]
 qs(ll, 0, len(ll) - 1)
 print(ll)
 
